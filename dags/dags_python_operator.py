cc5a0bdd1b4416d92475d8bc4d4ca71a7be640c8
ajnam <ppy611@gmail.com>
2024-09-05 18:00:00 +0900

    python_operator.py

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,30 @@
+
+from airflow import DAG
+import datetime
+import pendulum
+import random
+
+from airflow.operators.python import PythonOperator
+
+###### DAG 설정 코드
+with DAG(
+    dag_id="dags_python_operator",                           ## DAG 이름 8080 포트 콘솔화면에서 출력되는 이름, .py 파일 이름과 일치 시키기를 권장
+    schedule="0 0 * * *",                                  ## crontab 스케쥴 정보, 분 시 일 월 요일
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
+    catchup=False,                                         ## 시작시간과 현재 시간을 비교하여 누락 된 스케쥴 시간 만큼 수행 옵션, FALSE=사용안함 / TRUE=사용, TRUE시 누락분 만큼 병행 수행 됨 사용 하지 않길 권장
+    #dagrun_timeout=datetime.timedelta(minutes=60),
+    #tags=["example", "example2"],                         ## 이름 아래 작게 출력 되는 태그 이름
+    #params={"example_key": "example_value"},              ## DAG에서 사용하는 값 파라미터
+) as dag:
+    def select_fruit():
+        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
+        rand_int = random.randint(0,3)
+        print(fruit[rand_int])
+
+    py_t1 = PythonOperator(
+        task_id='py_t1'               ## task name
+        python_callable=select_fruit  ## 실행 하고자 하는 파이썬 함수
+    )
+
+
+    py_t1
