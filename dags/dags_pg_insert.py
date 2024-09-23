62f15f51f942497a5de41f2cce928d480ecc4250
ajnam <ppy611@gmail.com>
2024-09-23 18:42:38 +0900

    dags_pg_insert.py fin2

--- dags/dags_pg_insert.py
+++ dags/dags_pg_insert.py
@@ -21,15 +21,16 @@ with DAG(
         from contextlib import closing
         with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
             with closing(conn.cursor()) as cursor:
-                dag_id = kwargs.get('ti').dag_id
-                task_id = kwargs.get('ti').task_id
-                run_id = kwargs.get('ti').run_id
+                #dag_id = kwargs.get('ti').dag_id
+                #task_id = kwargs.get('ti').task_id
+                #run_id = kwargs.get('ti').run_id
                 msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
-                cursor.execute(sql,(dag_id,task_id,run_id,msg))
+                cursor.execute(sql,('dag_id','task_id','run_id','msg'))
                 conn.commit()
+
     pg_insert = PythonOperator(
         task_id='pg_insert',
         python_callable=insrt_postgres,
-        op_args=['localhost', '5432', 'ajnam', 'ajnam', 'ajnam']
+        op_args=['172.28.0.3', '5432', 'ajnam', 'ajnam', 'ajnam']
     )
