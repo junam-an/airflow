a4caf1e7252e791b47cd6f8ed5104688644054b0
ajnam <ppy611@gmail.com>
2024-09-23 17:30:33 +0900

    dags_pg_insert.py dag_id change repeat6

--- dags/dags_pg_insert.py
+++ dags/dags_pg_insert.py
@@ -10,7 +10,7 @@ from airflow.operators.python import PythonOperator
 with DAG(
     dag_id="dags_pg_insert",                           ## DAG 이름 8080 포트 콘솔화면에서 출력되는 이름, .py 파일 이름과 일치 시키기를 권장
     schedule="0 0 * * *",                                  ## crontab 스케쥴 정보, 분 시 일 월 요일
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
+    start_date=pendulum.datetime(2024, 12, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
     catchup=False,                                         ## 시작시간과 현재 시간을 비교하여 누락 된 스케쥴 시간 만큼 수행 옵션, FALSE=사용안함 / TRUE=사용, TRUE시 누락분 만큼 병행 수행 됨 사용 하지 않길 권장
     #dagrun_timeout=datetime.timedelta(minutes=60),
     #tags=["example", "example2"],                         ## 이름 아래 작게 출력 되는 태그 이름
@@ -31,7 +31,7 @@ with DAG(
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
             except Exception as e:
-                print(e.message)
+                print(str(e.message))
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
