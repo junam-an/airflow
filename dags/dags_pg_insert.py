6a07b728680db562e7963b9629cd6819af51ada7
ajnam <ppy611@gmail.com>
2024-09-23 20:12:40 +0900

    fin

--- dags/dags_pg_insert.py
+++ dags/dags_pg_insert.py
@@ -10,7 +10,7 @@ from airflow.operators.python import PythonOperator
 with DAG(
     dag_id="dags_pg_insert",                           ## DAG 이름 8080 포트 콘솔화면에서 출력되는 이름, .py 파일 이름과 일치 시키기를 권장
     schedule="0 0 * * *",                                  ## crontab 스케쥴 정보, 분 시 일 월 요일
-    start_date=pendulum.datetime(2024, 12, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
+    start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
     catchup=False,                                         ## 시작시간과 현재 시간을 비교하여 누락 된 스케쥴 시간 만큼 수행 옵션, FALSE=사용안함 / TRUE=사용, TRUE시 누락분 만큼 병행 수행 됨 사용 하지 않길 권장
     #dagrun_timeout=datetime.timedelta(minutes=60),
     #tags=["example", "example2"],                         ## 이름 아래 작게 출력 되는 태그 이름
@@ -21,16 +21,16 @@ with DAG(
         from contextlib import closing
         with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
             with closing(conn.cursor()) as cursor:
-                #dag_id = kwargs.get('ti').dag_id
-                #task_id = kwargs.get('ti').task_id
-                #run_id = kwargs.get('ti').run_id
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
                 msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,('dag_id','task_id','run_id','msg'))
                 conn.commit()
 
-    pg_insert = PythonOperator(
-        task_id='pg_insert',
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
         python_callable=insrt_postgres,
         op_args=['172.28.0.3', '5432', 'ajnam', 'ajnam', 'ajnam']
     )
