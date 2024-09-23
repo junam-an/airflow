8617eef7e36338712e8fb7f259691338e632137a
ajnam <ppy611@gmail.com>
2024-09-23 19:01:23 +0900

    sibal

--- dags/dags_pg_insert2.py
+++ dags/dags_pg_insert2.py
@@ -21,9 +21,9 @@ with DAG(
         from contextlib import closing
         with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
             with closing(conn.cursor()) as cursor:
-                dag_id = kwargs.get('ti').dag_id
-                task_id = kwargs.get('ti').task_id
-                run_id = kwargs.get('ti').run_id
+                dag_id = 'test1'
+                task_id = 'test2'
+                run_id = 'test3'
                 msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
                 cursor.execute(sql,(dag_id,task_id,run_id,msg))
