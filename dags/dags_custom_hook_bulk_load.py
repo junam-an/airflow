09c1d458471760a951fabf6b50984794d0932ac1
ajnam <ppy611@gmail.com>
2024-09-29 16:20:53 +0900

    dags/dags_custom_hook_bulk_load.py

--- dags/dags_custom_hook_bulk_load.py
+++ dags/dags_custom_hook_bulk_load.py
@@ -7,7 +7,7 @@ from hooks.custom_postgres_hook import CustomPostgresHook
 
 ###### DAG 설정 코드
 with DAG(
-    dag_id="dags_pg_insert_hook",                           ## DAG 이름 8080 포트 콘솔화면에서 출력되는 이름, .py 파일 이름과 일치 시키기를 권장
+    dag_id="dags_custom_hook_bulk_load",                           ## DAG 이름 8080 포트 콘솔화면에서 출력되는 이름, .py 파일 이름과 일치 시키기를 권장
     schedule="0 0 * * *",                                  ## crontab 스케쥴 정보, 분 시 일 월 요일
     start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),    ## DAG 시작 시간 정보(언제 부터 수행 할 건지)
     catchup=False,                                         ## 시작시간과 현재 시간을 비교하여 누락 된 스케쥴 시간 만큼 수행 옵션, FALSE=사용안함 / TRUE=사용, TRUE시 누락분 만큼 병행 수행 됨 사용 하지 않길 권장
