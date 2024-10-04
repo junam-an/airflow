80689a5e0ecf7a79358c75c26620eb763cbc3661
ajnam <ppy611@gmail.com>
2024-10-04 19:37:38 +0900

    t8

--- plugins/common/common_execute_pre.py
+++ plugins/common/common_execute_pre.py
@@ -21,23 +21,24 @@ class CustomPostgresHook(BaseHook):
         self.log.info(self.dbname)
         self.log.info(self.port)
 
-        #self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
+        self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
         #return self.postgres_conn
     
-        from contextlib import closing
-        with closing(psycopg2.connect(host=self.host, dbname=self.dbname, user=self.user, password=self.password, port=int(self.port))) as conn:
-            with closing(conn.cursor()) as conn:
-                self.log.info(f'log table data_interval_start insert')
-                sql = "insert into af_task_log values ('test','test', to_char(now(), 'YYYYMMDDHH24MISS'),NULL,NULL,to_char(now(), 'YYYYMMDDHH24MISS'));"
-                self.log.info(sql)
+        #from contextlib import closing
+        #with closing(psycopg2.connect(host=self.host, dbname=self.dbname, user=self.user, password=self.password, port=int(self.port))) as conn:
+        #    with closing(conn.cursor()) as conn:
+        self.log.info(f'log table data_interval_start insert')
+        sql = "insert into af_task_log values ('test','test', to_char(now(), 'YYYYMMDDHH24MISS'),NULL,NULL,to_char(now(), 'YYYYMMDDHH24MISS'));"
+        self.log.info(sql)
 
 
-                try:
-                    self.log.info(f'insert를 시작 합니다.')
-                    conn.execute(sql)
-                    conn.commit()
-                except:
-                    self.log.info(f'insert 에 실패 하였습니다')
+        try:
+            self.log.info(f'insert를 시작 합니다.')
+            cursor = self.postgres_conn.cursor()
+            cursor.execute(sql)
+            self.postgres_conn.commit()
+        except:
+            self.log.info(f'insert 에 실패 하였습니다')
         
         return True
         
