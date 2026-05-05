60e21c2f6f4e96cd84b2800a3e69083b6e651655
ajnam <ppy611@gmail.com>
2026-05-05 21:51:10 +0900

    asdasd

---
+++ dags/dags_static_SALES_ORACLE_TO_ORACLE_ETL.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+from common.static_odbc_oracle_to_oracle_etl_minute import create_odbc_to_odbc_meta_task
+
+
+DAG_ID = "STATIC_ORACLE_TO_ORACLE_ETL_META_3"
+
+
+with DAG(
+    dag_id=DAG_ID,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["etl", "postgres", "static"],
+) as dag:
+
+    @task(task_id="TASK1")
+    def TASK1(**context):
+        return create_odbc_to_odbc_meta_task(
+            dag_id="DYNAMIC_ORACLE_TO_ORACLE_ETL_META_3",
+            task_name="TASK1",
+            **context,
+        )
+
+    @task(task_id="TASK2")
+    def TASK2(**context):
+        return create_odbc_to_odbc_meta_task(
+            dag_id="DYNAMIC_ORACLE_TO_ORACLE_ETL_META_3",
+            task_name="TASK2",
+            **context,
+        )
+
+
+    v_TASK1 = TASK1()
+    v_TASK2 = TASK2()
+
+    v_TASK1 >> v_TASK2
